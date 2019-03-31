00435f78c8c7a57a12bdd3ed33d383f06a2b7107
Vicentiu Galanopulo <viceniu@balena.io>
2019-03-31 21:35:28 +0300

    run.sh: Fix path for 8188eu.ko
    Signed-off-by: Vicentiu Galanopulo <viceniu@balena.io>

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ OS_VERSION=$(echo $RESIN_HOST_OS_VERSION | cut -d " " -f 2)
 echo "OS Version is $OS_VERSION"
 
 rmmod r8188eu
-insmod ./8188eu.ko
+insmod ./rtl8188eu/8188eu.ko
 ./hostapd-2.7/hostapd/hostapd -B ./hostapd.conf
 
 while true; do
