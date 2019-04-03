c910f532e11d18857db5db85a75a6c1721f02ef6
Vicentiu Galanopulo <viceniu@balena.io>
2019-04-03 14:10:36 +0300

    Removed kernel module and updated run.sh and build.sh
    The kernel module has been added to the yocto build
    
    Signed-off-by: Vicentiu Galanopulo <viceniu@balena.io>

--- build.sh
+++ build.sh
@@ -16,10 +16,3 @@ push ./hostapd-2.7/hostapd
 make
 pop
 
-wget https://misc1.dev.resin.io/~vicgal/kernel-modules-headers/fin_cm3/v2.26.0+rev1/kernel_modules_headers.tar.gz
-tar xvf kernel_modules_headers.tar.gz -C .
-
-rm -rf kernel_modules_headers.tar.gz 
-
-cd rtl8188eu
-make all
--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 OS_VERSION=$(echo $RESIN_HOST_OS_VERSION | cut -d " " -f 2)
 echo "OS Version is $OS_VERSION"
 
-rmmod r8188eu
-insmod ./rtl8188eu/8188eu.ko
 ./hostapd-2.7/hostapd/hostapd -B ./hostapd.conf
 
 while true; do
