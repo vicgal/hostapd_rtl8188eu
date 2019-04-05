426bba2725112416356a5f1a1b89998c5b3c722c
Vicentiu Galanopulo <viceniu@balena.io>
2019-04-05 12:34:08 +0300

    Moved hostapd to Dockerfile.template
    Signed-off-by: Vicentiu Galanopulo <viceniu@balena.io>

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 OS_VERSION=$(echo $RESIN_HOST_OS_VERSION | cut -d " " -f 2)
 echo "OS Version is $OS_VERSION"
 
-./hostapd-2.7/hostapd/hostapd -B ./hostapd.conf
+hostapd -B ./hostapd.conf
 
 while true; do
 	sleep 60
